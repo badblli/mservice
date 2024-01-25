4bac5f9a64b39c6ea532c97c0804366d320c693c
sahinbrn <sahinbaran077@gmail.com>
2024-01-25 10:58:48 +0300

    languages page added

--- src/router/MainRoutes.ts
+++ src/router/MainRoutes.ts
@@ -35,6 +35,11 @@ const MainRoutes = {
             path: '/dashboards/settings/selectitemcategory',
             component: () => import('@/views/dashboards/settings/SelectItemCategory.vue')
         },
+        {
+            name: 'Languages',
+            path: '/dashboards/settings/languages',
+            component: () => import('@/views/dashboards/settings/Languages.vue')
+        },
 
         /////////////////////////////////////////////////////////////////////////////
         {
